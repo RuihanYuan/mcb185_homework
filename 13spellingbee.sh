19878850e8e0ff83fed47eb1802d935ff7bdcfb9
RuihanYuan <rhyuan@ucdavis.edu>
2024-01-18 23:13:18 -0800

    author update

--- 13spellingbee.sh
+++ 13spellingbee.sh
@@ -2,6 +2,8 @@
 #1 char in the middle has to be used every time
 #6 outer char can be used any number of times
 #word_length min is 4
+#Author: Lisa, Henry
+
 #1.grep the letter only with r
 #2.grep the word with only the letter:z,o,n,i,a,c; and length must exceed 4
 gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "r" | grep -E "^[zoniacr]{4,}$" 
