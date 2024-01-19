625925bca56cc7db81dad7dba1d6c66cd38718b7
RuihanYuan <rhyuan@ucdavis.edu>
2024-01-18 23:09:45 -0800

    unit1 hw upload

---
+++ 10env.sh
@@ -0,0 +1,6 @@
+#reports the following environment variables and their contents
+echo "Hello, $USER"
+echo "Your home directory is: $HOME"
+echo "Your default shell is: $SHELL"
+echo "Your current working directory is: $PWD"
+echo "Your system's PATH is: $PATH"
---
+++ 13spellingbee.sh
@@ -0,0 +1,7 @@
+#7 letters total
+#1 char in the middle has to be used every time
+#6 outer char can be used any number of times
+#word_length min is 4
+#1.grep the letter only with r
+#2.grep the word with only the letter:z,o,n,i,a,c; and length must exceed 4
+gunzip -c ~/Code/MCB185/data/dictionary.gz | grep "r" | grep -E "^[zoniacr]{4,}$" 
