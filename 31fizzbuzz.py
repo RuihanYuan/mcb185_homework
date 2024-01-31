2fd506af47297ffb04a033349818f6e9fa1a3cf1
RuihanYuan <rhyuan@ucdavis.edu>
2024-01-30 20:58:31 -0800

    update

--- 31fizzbuzz.py
+++ 31fizzbuzz.py
@@ -4,11 +4,11 @@
 # FizzBuzz puzzle
 
 for num in range(1, 101):
-		if num % 3 ==0 and num % 5 == 0:
-				print('FizzBuzz')
-		elif num % 3 == 0:
-				print('Fizz')
-		elif num % 5 == 0:
-				print('Buzz')
-		else:
-				print(num)
+	if num % 3 ==0 and num % 5 == 0:
+		print('FizzBuzz')
+	elif num % 3 == 0:
+		print('Fizz')
+	elif num % 5 == 0:
+		print('Buzz')
+	else:
+		print(num)
