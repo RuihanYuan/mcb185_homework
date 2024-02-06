f261a01f59322fcae37b842ef16a1a64045cd3fe
RuihanYuan <rhyuan@ucdavis.edu>
2024-02-06 09:28:25 -0800

    update

--- 38quiz.py
+++ 38quiz.py
@@ -1,7 +1,21 @@
 #author: Lisa Yuan, Francessca Asuncion
+
+
 def pi_cal(n):
 	for i in range(n):
 		pi = 4 * (-1)**(i) / (2 * i + 1)
 	return pi
 	
-print(pi_cal(5))
+
+
+def pi_calc(n):
+	pi = 3
+	# sign = sign*-1 to alternative
+	sign = 1
+	for i in range(2, n, 2):
+		pi += sign * (4 / (i * (i + 1) * (i + 2)))
+		sign *= -1
+	return pi
+	
+for i in range(1,100):
+	print(pi_cal(i),pi_calc(i))
